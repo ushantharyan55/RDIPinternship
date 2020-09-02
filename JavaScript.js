5aaf123b73e60ae2e7899476a63d4fde08002c09
ushantharyan55 <70165516+ushantharyan55@users.noreply.github.com>
2020-09-02 16:48:37 +0530

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,1 @@
+document.write("JavaScript is a programming language used most often for dynamic client-side scripts on webpages, but it is also often used on the server-side, using a runtime such as Node.js.");
